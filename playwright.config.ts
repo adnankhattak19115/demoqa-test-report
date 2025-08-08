e5ca29575baae77ecb0728fd416746f2f7ae4f6b
ADNAN KHATTAK <ADNANMALIK@BAGH.MTBC.COM>
2025-08-08 16:31:13 +0500

    Increase loading time for test cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 60000,
+  timeout: 90000,
   // workers: 1,
   use: {
     headless: true, // 👈 show browser
@@ -12,8 +12,8 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
-    actionTimeout: 15000, // optional: individual action timeout
-    navigationTimeout: 30000,
+    actionTimeout: 30000, // optional: individual action timeout
+    navigationTimeout: 50000,
   },
   reporter: [['html', { outputFolder: 'playwright-report' }], ['allure-playwright'], ['list'],],
 });
