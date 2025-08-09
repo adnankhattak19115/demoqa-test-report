5f8b11011c001133e3e36ab337b000f774c2c04c
adnankhattak19115 <adnankhattak19115@users.noreply.github.com>
2025-08-09 11:18:03 +0000

    deploy: a0bf21c73d4f5f155b5a2bb933efca2c17857bff

--- locators/homePageLocators.ts
+++
@@ -1,3 +0,0 @@
-export const homePageLocators = {
-  elementsCard: "//h5[normalize-space()='Elements']",
-};
--- tests/broken-links-image.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test";
-import { navigateToBrokenLinks, checkValidImage, checkBrokenImage } from "../pages/elementsPage";
-
-test("Check valid and broken images", async ({ page }) => {
-  await page.goto("https://demoqa.com/");
-  await navigateToBrokenLinks(page);
-  await checkValidImage(page);
-  await checkBrokenImage(page);
-});
